30359f24028e17fe3ebe86f7b8fe2d767d6b1190
Win Cowger, PhD <wincowger@gmail.com>
2025-08-26 19:14:15 -0700

    remove R files

--- global.R
+++ global.R
@@ -1,36 +1,67 @@
+# Check for Auth Tokens and setup, you can change these to test the triggering
+# of functions without removing the files.
+translate <- file.exists("www/googletranslate.html")
+
+#remotes::install_github("wincowgerDEV/OpenSpecy-package@vignettes")
+
+# Libraries ----
 library(shiny)
-library(shinyWidgets)
-library(bslib)
-library(caTools)
-library(data.table)
-library(glmnet)
-library(hyperSpec)
-library(mmand)
-library(plotly)
-library(signal)
-library(bs4Dash)
-library(digest)
 library(shinyjs)
+library(shinyWidgets)
 library(dplyr)
-library(shinyBS)
-library(jsonlite)
-library(OpenSpecy)
+library(plotly)
+library(data.table)
 library(DT)
+library(digest)
+#library(curl)
+#library(loggit)
+library(bs4Dash)
+library(ggplot2)
+library(reshape2)
 
+library(OpenSpecy)
+#library(glmnet)
 
-load_data <- function() {
-  data("raman_hdpe")
-  testdata <- data.table(wavenumber = raman_hdpe$wavenumber,
-                         intensity = raman_hdpe$spectra$intensity)
-  
-  # Inject variables into the parent environment
-  invisible(list2env(as.list(environment()), parent.frame()))
+# Define the custom theme
+theme_black_minimal <- function(base_size = 11, base_family = "") {
+    theme_minimal(base_size = base_size, base_family = base_family) +
+        theme(
+            plot.background = element_rect(fill = "black", color = NA),
+            panel.background = element_rect(fill = "black", color = NA),
+            panel.grid.major = element_line(color = "white"),
+            panel.grid.minor = element_line(color = "white"),
+            axis.line = element_line(color = "white"),
+            axis.ticks = element_line(color = "white"),
+            axis.text = element_text(color = "white"),
+            axis.title = element_text(color = "white"),
+            plot.title = element_text(color = "white", hjust = 0.5),
+            plot.subtitle = element_text(color = "white", hjust = 0.5),
+            plot.caption = element_text(color = "white"),
+            legend.text = element_text(color = "white"),
+            legend.title = element_text(color = "white"),
+            legend.background = element_rect(fill = "black"),
+            legend.key = element_rect(fill = "black"),
+            strip.background = element_rect(fill = "black", color = NA),
+            strip.text = element_text(color = "white")
+        )
 }
 
+# Load all data ----
+load_data <- function() {
+    data("raman_hdpe")
+    
+    testdata <-  data.table(wavenumber = raman_hdpe$wavenumber, 
+                            intensity = raman_hdpe$spectra$intensity)
+    
+    # Inject variables into the parent environment
+    invisible(list2env(as.list(environment()), parent.frame()))
+}
+
+# Name keys for human readable column names ----
 
 version <- paste0("Open Specy v", packageVersion("OpenSpecy"))
 citation <- HTML(
-  'Cowger, W., Karapetrova, A., Lincoln, C., Chamas, A., Sherrod, H., Leong, N., Lasdin, K. S., 
+    'Cowger, W., Karapetrova, A., Lincoln, C., Chamas, A., Sherrod, H., Leong, N., Lasdin, K. S., 
   Knauss, C., Teofilović, V., Arienzo, M. M., Steinmetz, Z., Primpke, S., 
   Darjany, L., Murphy-Hagan, C., Moore, S., Moore, C., Lattin, G., 
   Gray, A., Kozloski, R., Bryksa, J., Maurer, B. (2025). 
@@ -42,31 +73,32 @@ citation <- HTML(
 
 # Workaround for Chromium Issue 468227
 downloadButton <- function(...) {
-  tag <- shiny::downloadButton(...)
-  tag$attribs$download <- NULL
-  tag
+    tag <- shiny::downloadButton(...)
+    tag$attribs$download <- NULL
+    tag
 }
 
 # Define the custom theme
 theme_black_minimal <- function(base_size = 11, base_family = "") {
-  theme_minimal(base_size = base_size, base_family = base_family) +
-    theme(
-      plot.background = element_rect(fill = "black", color = NA),
-      panel.background = element_rect(fill = "black", color = NA),
-      panel.grid.major = element_line(color = "white"),
-      panel.grid.minor = element_line(color = "white"),
-      axis.line = element_line(color = "white"),
-      axis.ticks = element_line(color = "white"),
-      axis.text = element_text(color = "white"),
-      axis.title = element_text(color = "white"),
-      plot.title = element_text(color = "white", hjust = 0.5),
-      plot.subtitle = element_text(color = "white", hjust = 0.5),
-      plot.caption = element_text(color = "white"),
-      legend.text = element_text(color = "white"),
-      legend.title = element_text(color = "white"),
-      legend.background = element_rect(fill = "black"),
-      legend.key = element_rect(fill = "black"),
-      strip.background = element_rect(fill = "black", color = NA),
-      strip.text = element_text(color = "white")
-    )
+    theme_minimal(base_size = base_size, base_family = base_family) +
+        theme(
+            plot.background = element_rect(fill = "black", color = NA),
+            panel.background = element_rect(fill = "black", color = NA),
+            panel.grid.major = element_line(color = "white"),
+            panel.grid.minor = element_line(color = "white"),
+            axis.line = element_line(color = "white"),
+            axis.ticks = element_line(color = "white"),
+            axis.text = element_text(color = "white"),
+            axis.title = element_text(color = "white"),
+            plot.title = element_text(color = "white", hjust = 0.5),
+            plot.subtitle = element_text(color = "white", hjust = 0.5),
+            plot.caption = element_text(color = "white"),
+            legend.text = element_text(color = "white"),
+            legend.title = element_text(color = "white"),
+            legend.background = element_rect(fill = "black"),
+            legend.key = element_rect(fill = "black"),
+            strip.background = element_rect(fill = "black", color = NA),
+            strip.text = element_text(color = "white")
+        )
 }
+
